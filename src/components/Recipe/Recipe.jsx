ebca36d0657b787b1b89c2508c6e44d35fc2643d
AdnaNation <azizulislamadnan@gmail.com>
2024-03-16 17:49:37 +0600

    recipes card added

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,11 +1,28 @@
 import PropTypes from 'prop-types';
 
 const Recipe = ({recipe}) => {
-    const{recipe_img}= recipe
+    const{recipe_img, recipe_name, short_description, ingredients}= recipe
     return (
         <div>
-            <img src={recipe_img} alt="" />
+            
+            <div className="card  bg-base-100 shadow-xl p-4">
+  <figure><img className='w-full' src={recipe_img} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">{recipe_name}</h2>
+    <p>{short_description}</p>
+    <hr />
+    {
+      ingredients.map((ingredient, idx)=> <span key={idx}>{ingredient}</span>)
+    }
+
+
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
         </div>
+        
     );
 };
 
