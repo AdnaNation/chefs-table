4748de241c01da354005c7feb1d727d5b2abe2a5
AdnaNation <azizulislamadnan@gmail.com>
2024-03-16 14:38:43 +0600

    nav bar added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import { FaRegCircleUser } from "react-icons/fa6";
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex="0" role="button" className="btn btn-ghost  lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16" />
+                        </svg>
+                    </div>
+                    <ul tabIndex="0"
+                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Home</a></li>
+                        <li><a>Recipes</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                        
+                        <li><a className="text-[#89898B] mr-5"><i className="fa-solid fa-magnifying-glass"></i></a></li>
+                    </ul>
+                </div>
+                <a className="text-lg lg:text-4xl font-extrabold manrope">Recipe Calories</a>
+            </div>
+            <div className="navbar-center hidden lg:flex md:flex">
+                <ul className="menu menu-horizontal px-1 text-[#150B2BB3]">
+                    
+                    <li><a>Home</a></li>
+                        <li><a>Recipes</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+
+                </ul>
+            </div>
+            <div className="navbar-end">
+            <input type="text" placeholder="Search" className="input input-bordered rounded-full mr-2 w-full max-w-xs hidden md:block lg:block bg-[#150B2B0D]" />
+                <a className="bg-[#0BE58A] text-white font-bold manrope text-3xl lg:text-5xl rounded-full"><FaRegCircleUser></FaRegCircleUser></a>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default Header;
